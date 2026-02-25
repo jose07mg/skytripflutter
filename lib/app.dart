7437f34688bbc012d0bee7e767469874c77471be
jesussl.11 <jsanchez@alu.fpsanantonio.com>
2026-02-25 12:46:54 +0100

    gastos_v1

--- lib/app.dart
+++ lib/app.dart
@@ -4,6 +4,7 @@ import 'package:rms/shared/themes/app_theme.dart';
 import 'core/constants/routes.dart';
 import 'features/login/login.dart';
 import 'features/home/home.dart';
+import 'features/gastos/gastos.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -19,6 +20,7 @@ class MyApp extends StatelessWidget {
       routes: {
         Routes.login: (context) => const LoginPage(),
         Routes.home: (context) => const HomePage(),
+        Routes.gastos: (context) => const GastosPage(),
       },
     );
   }
--- lib/core/constants/routes.dart
+++ lib/core/constants/routes.dart
@@ -2,4 +2,5 @@ class Routes {
   static const login = '/login';
   static const register = '/register';
   static const home = '/home';
+  static const gastos = '/gastos';
 }
