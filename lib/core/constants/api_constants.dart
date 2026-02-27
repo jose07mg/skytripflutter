525c46a9f4731ce55728ea9fb96be7cdf9b782c9
laquizhpe <laquizhpe@alu.ucam.edu>
2026-02-27 12:03:11 +0100

    feat: introduce `ApiConstants` class for base URL and remove it from gitignore

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,9 +1,9 @@
 class ApiConstants {
-  // RENOMBRAME A api_constants.dart
+  // IMPORTANTE: Este archivo está en el .gitignore.
   // Cambia esta URL base cuando estés debuggeando en un dispositivo físico.
   // Por ejemplo, para un celular físico usa la IP de tu PC: 'http://192.168.X.X/RMSmira_api/public'
   // Para emulador Android: 'http://10.0.2.2/RMSmira_api/public'
   // Para emulador iOS o Web: 'http://localhost/RMSmira_api/public'
 
-  static const String baseUrl = 'https://tu-dominio-en-produccion.com/api';
+  static const String baseUrl = 'http://localhost/RMSmira_api/public';
 }
