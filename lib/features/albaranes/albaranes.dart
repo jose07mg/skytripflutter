ee7234a6897eef7397a298c3ac2ae547d81336ef
jorgecascales88 <jcascales8@alu.fpsanantonio.com>
2026-03-02 13:58:46 +0100

    fix/alabaranes-estados

--- lib/features/albaranes/albaranes.dart
+++ lib/features/albaranes/albaranes.dart
@@ -314,8 +314,8 @@ class AlbaranCard extends StatelessWidget {
                         ),
                         decoration: BoxDecoration(
                           color: estado == 'Pendiente'
-                              ? Colors.orange.withOpacity(0.2)
-                              : Colors.green.withOpacity(0.2),
+                              ? Colors.orange.withValues(alpha: 0.2)
+                              : Colors.green.withValues(alpha: 0.2),
                           borderRadius: BorderRadius.circular(12),
                           border: Border.all(
                             color: estado == 'Pendiente'
